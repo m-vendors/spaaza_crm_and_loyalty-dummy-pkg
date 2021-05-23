caa8c9c3238ab723675f9878f0b78a363c1f1573
M Vendors <m-vendors@protonmail.com>
2021-05-23 20:50:47 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'spaaza_crm_and_loyalty_DummyPkg',
+    __DIR__
+);
